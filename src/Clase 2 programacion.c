f6a3dad24e068177e7d6b713e6222c63373f9011
Lucianosanjurjo <luchosanjurjo@gmail.com>
2021-08-26 21:42:04 -0300

    primer commit

---
+++ src/Clase 2 programacion.c
@@ -0,0 +1,46 @@
+/*
+ ============================================================================
+ Name        : Clase.c
+ Author      : Luciano
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+
+
+//Bucles
+//while
+//for
+//do while
+//cualquiera se puede usar para cualquier cosa
+//son equivalentes
+
+//Si se cuantas veces voy a iterar va un FOR
+//si no se WHILE
+//cuando primero quiero iterar y despues preguntar por la condicion DO WHILE
+
+char respuesta;
+
+do
+{
+	printf("continuar?s-n");
+	scanf("%c", &respuesta);
+}
+while(respuesta=='s');
+
+
+
+
+
+
+
+
+	return EXIT_SUCCESS;
+}
